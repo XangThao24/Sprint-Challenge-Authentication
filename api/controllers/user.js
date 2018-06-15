e11fc952b2a3ce32b222268fb72434e4c51cee29
Xang Thao <xangthao24@yahoo.com>
2018-06-15 09:14:16 -0700

    Able to create new users from user controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,8 +5,16 @@ const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User.create({username, password})
+    .then(({username, id}) => {
+      res.status(201).json({username, id});
+    })
+    .catch(err => {
+      res.status(500).json(err)
+    })
 };
 
 module.exports = {
   createUser
 };
+
